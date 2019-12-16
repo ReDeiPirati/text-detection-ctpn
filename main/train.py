b67fdf66559bc446dde7141d8684e5661af50f27
root <agozzoli.ai@gmail.com>
2019-12-16 23:00:36 +0000

    Adding mIoU and plotting bboxes into Tensorboard

--- main/train.py
+++ main/train.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import sys
 import time
+import cv2
 
 import tensorflow as tf
 import numpy as np
@@ -29,6 +30,81 @@ tf.app.flags.DEFINE_integer('save_checkpoint_steps', 2000, '')
 tf.app.flags.DEFINE_string('data_folder', '/floyd/input/checkpoints_mlt', '')
 tf.app.flags.DEFINE_string('data_eval_folder', '/floyd/input/naapf', '')
 FLAGS = tf.app.flags.FLAGS
+
+def get_iou(bb1, bb2):
+    """
+    Calculate the Intersection over Union (IoU) of two bounding boxes.
+
+    Parameters
+    ----------
+    bb1 : dict
+        Keys: {'x1', 'x2', 'y1', 'y2'}
+        The (x1, y1) position is at the top left corner,
+        the (x2, y2) position is at the bottom right corner
+    bb2 : dict
+        Keys: {'x1', 'x2', 'y1', 'y2'}
+        The (x, y) position is at the top left corner,
+        the (x2, y2) position is at the bottom right corner
+
+    Returns
+    -------
+    float
+        in [0, 1]
+    """
+    assert bb1['x1'] < bb1['x2']
+    assert bb1['y1'] < bb1['y2']
+    assert bb2['x1'] < bb2['x2']
+    assert bb2['y1'] < bb2['y2']
+
+    # determine the coordinates of the intersection rectangle
+    x_left = max(bb1['x1'], bb2['x1'])
+    y_top = max(bb1['y1'], bb2['y1'])
+    x_right = min(bb1['x2'], bb2['x2'])
+    y_bottom = min(bb1['y2'], bb2['y2'])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
+    bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
+
+
+def plain_iou(bboxes1, bboxes2):
+        x11, y11, x12, y12 = np.split(bboxes1, 4, axis=0)
+        x21, y21, x22, y22 = np.split(bboxes2, 4, axis=0)
+        
+        # Sanity check
+        assert x11 <= x12
+        assert y11 <= y12
+        assert x21 <= x22
+        assert y21 <= y22
+        
+        xA = np.maximum(x11, np.transpose(x21))
+        yA = np.maximum(y11, np.transpose(y21))
+        xB = np.minimum(x12, np.transpose(x22))
+        yB = np.minimum(y12, np.transpose(y22))
+        interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
+        boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
+        boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
+        iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
+
+        # Sanity check
+        assert iou >= 0.0
+        assert iou <= 1.0
+        return iou
 
 
 def main(argv=None):
@@ -77,8 +153,8 @@ def main(argv=None):
                                                              ignore_missing_vars=True)
 
     steps_in_epoch = data_provider.get_steps_in_epoch(FLAGS.data_folder)
-    epoch_counter = 0
-
+    epoch = 0
+    
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.gpu_options.per_process_gpu_memory_fraction = 0.95
@@ -120,17 +196,35 @@ def main(argv=None):
                 filename = os.path.join(FLAGS.checkpoint_path, filename)
                 saver.save(sess, filename)
                 print('Write model to: {:s}'.format(filename))
-
-
+                
             if step % steps_in_epoch == 0:
-                data_eval_generator = data_provider.get_batch(FLAGS.data_eval_folder, num_workers=FLAGS.num_readers)
-                gt_bboxes = []
-                pred_bboxes = []
+                epoch += 1
+
+            
+#             if step % steps_in_epoch == 0:
+            if step % 2000 == 0:
+                data_eval_generator = data_provider.get_batch(FLAGS.data_eval_folder, num_workers=FLAGS.num_readers)   
+                iou_list = []
+                
                 for it in range(steps_in_epoch):
+                    gt_bboxes = []
+                    pred_bboxes = []
                     data = next(data_eval_generator)
-                    bbox_pred_val, cls_prob_val = sess.run([bbox_pred, cls_prob],
+                    
+                    # Adding Ground Truth Label
+                    img_to_log = data[0][0]
+                    for p in data[1]:
+                        cv2.rectangle(img_to_log, (p[0], p[1]), (p[2], p[3]), color=(0, 0, 255), thickness=1)
+                    
+                     # Debugging
+                    img_to_log= np.reshape(img_to_log, (-1, 228, 228, 3))
+                    image_op = tf.summary.image("Eval data", img_to_log)
+                    
+                    bbox_pred_val, cls_prob_val, image_str = sess.run([bbox_pred, cls_prob, image_op],
                                                        feed_dict={input_image: data[0],
                                                                   input_im_info: data[2]})
+                    
+                    summary_writer.add_summary(image_str, global_step=step)
                     
                     textsegs, _ = proposal_layer(cls_prob_val, bbox_pred_val, data[2])
                     scores = textsegs[:, 0]
@@ -139,27 +233,38 @@ def main(argv=None):
                     textdetector = TextDetector(DETECT_MODE='H')
                     
                     img = data[0][0]
+                    img_to_log_w_bboxes = data[0][0]
                     boxes = textdetector.detect(textsegs, scores[:, np.newaxis], img.shape[:2])
                     boxes = np.array(boxes, dtype=np.int)
+                    for b in boxes:
+                        pred_bboxes.append([b[0], b[1], b[6], b[7]])
+                        cv2.rectangle(img_to_log_w_bboxes, (b[0], b[1]), (b[2], b[3]), color=(255, 0, 0), thickness=1)
                     
-                    points = data[1]
-                    gt_bboxes.append([points[0], points[1], points[2], points[3]])
-                    pred_bboxes.append(boxes)
+                    tuple_points = data[1]
+                    for point in tuple_points:
+                        gt_bboxes.append([point[0], point[1], point[2], point[3]])
                     
-                    if it == 0:
-                        print(np.array(gt_bboxes, dtype=np.int), pred_bboxes)
-                
+                     # Debugging
+                    img_to_log2 = np.reshape(img_to_log_w_bboxes, (-1, 228, 228, 3))
+                    image_op2 = tf.summary.image("Eval data w/ bbox", img_to_log2)
                     
-#                 mIoU, update_op = tf.metrics.mean_iou(pred_bboxes, bboxes, num_classes=1, weights=None)
-#                 iou = sess.run(mIoU)
-#                 tf.summary.scalar('meanIoU', mIoU)
+                    image_str2 = sess.run(image_op2)
+                    summary_writer.add_summary(image_str2, global_step=step)
+                    
 
-                from sklearn.metrics import jaccard_similarity_score
-                iou = jaccard_similarity_score(pred_bboxes, gt_bboxes)
+                    # Extend list to meet the one for gt
+                    if len(pred_bboxes) == 0 or len(gt_bboxes) > len(pred_bboxes):
+                        pred_bboxes = pred_bboxes[:len(gt_bboxes)] + [[0, 0, 0, 0]]*(len(gt_bboxes) - len(pred_bboxes))
 
-                print('Epoch {}, IoU score = {:.3f})'.format(epoch_counter, iou))
-                epoch_counter += 1
-
+  
+                    for bb_gt, bb_pred in zip(gt_bboxes, pred_bboxes):
+                        bb1 = np.array(bb_gt, dtype=np.int)
+                        bb2 = np.array(bb_pred, dtype=np.int)
+                        iou_list.append(plain_iou(np.array(bb1, dtype=np.int), np.array(bb2, dtype=np.int)))
+                            
+                       
+                print('# Eval # Step {:06d}, Epoch {:03d}, mIoU score = {:.3f}'.format(step, epoch, np.average(iou_list)))
+                        
 
 if __name__ == '__main__':
     tf.app.run()
